6fd6148154d181e58e83576dff9523feb0197cd4
Jimena Arnaiz <jimeclaudeen@gmail.com>
2025-03-04 12:39:26 +0100

    Configuración de la API Spoonacular. Definiicón de métodos iniciales en SpoonacularApi para obtener recetas, ingredientes... lo que creo voy a ir necesitando a medida que avance.

--- app/src/main/java/com/example/recipeat/data/api/RetrofitClient.kt
+++ app/src/main/java/com/example/recipeat/data/api/RetrofitClient.kt
@@ -1,3 +1,17 @@
 package com.example.recipeat.data.api
 
-//TODO
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL = "https://api.spoonacular.com/"
+
+    // Creamos un cliente Retrofit con la base URL y el convertidor Gson para parsear la respuesta JSON
+    val api: SpoonacularApi by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL) // La URL base de la API
+            .addConverterFactory(GsonConverterFactory.create()) // Usamos Gson para parsear las respuestas JSON
+            .build()
+            .create(SpoonacularApi::class.java) // Creamos la instancia de la interfaz de la API
+    }
+}
--- app/src/main/java/com/example/recipeat/data/api/SpoonacularApi.kt
+++ app/src/main/java/com/example/recipeat/data/api/SpoonacularApi.kt
@@ -1,6 +1,64 @@
 package com.example.recipeat.data.api
 
-interface SpoonacularAPI {
-    //TODO
+import com.example.recipeat.data.model.RandomRecipesResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface SpoonacularApi {
+
+//    //  Buscar recetas por ingredientes
+//    @GET("recipes/findByIngredients")
+//    suspend fun buscarRecetasPorIngredientes(
+//        @Query("ingredients") ingredientes: String,
+//        @Query("number") number: Int = 10,
+//        @Query("apiKey") apiKey: String = "ec231e7612fa4dd399b9e2f2c0f9bcc8"
+//    ): List<ApiReceta>
+//
+//    // Obtener detalles de una receta por ID
+//    @GET("recipes/{id}/information")
+//    suspend fun obtenerDetallesReceta(
+//        @Path("id") recetaId: Int,
+//        @Query("includeNutrition") includeNutrition: Boolean = false,
+//        @Query("apiKey") apiKey: String = "ec231e7612fa4dd399b9e2f2c0f9bcc8"
+//    ): ApiReceta
+//
+//    // Buscar ingredientes por nombre autocomplete
+//    @GET("food/ingredients/autocomplete")
+//    suspend fun buscarIngredientesAutocompletado(
+//        @Query("query") ingredient: String,
+//        @Query("number") number: Int = 7,
+//        @Query("apiKey") apiKey: String = "ec231e7612fa4dd399b9e2f2c0f9bcc8"
+//    ): List<Ingrediente>
+//
+//    // Buscar recetas por nombre
+//    @GET("recipes/complexSearch")
+//    suspend fun buscarRecetasPorNombre(
+//        @Query("query") name: String,
+//        @Query("number") number: Int = 50, // num de resultados
+//        @Query("apiKey") apiKey: String = "ec231e7612fa4dd399b9e2f2c0f9bcc8"
+//    ): List<ApiReceta>
+//
+//    @GET("recipes/autocomplete")
+//    suspend fun buscarRecetasPorNombreAutocompletado(
+//        @Query("query") recipe: String,
+//        @Query("number") number: Int = 7,
+//        @Query("apiKey") apiKey: String = "ec231e7612fa4dd399b9e2f2c0f9bcc8"
+//    ): List<ApiReceta>
+//
+//    // Obtener recetas similares a una receta por ID
+//    @GET("recipes/{id}/similar")
+//    suspend fun obtenerRecetasSimilares(
+//        @Path("id") recetaId: Int,
+//        @Query("number") number: Int = 10,
+//        @Query("apiKey") apiKey: String = "ec231e7612fa4dd399b9e2f2c0f9bcc8"
+//    ): List<ApiReceta>
+
+    // Obtener recetas random
+    @GET("recipes/random")
+    suspend fun obtenerRecetasRandom(
+        @Query("number") number: Int = 100,
+        @Query("apiKey") apiKey: String = "ec231e7612fa4dd399b9e2f2c0f9bcc8"
+    ): RandomRecipesResponse
 
 }
