26f79d2a00d8b081a877a055203589b1dd7c945a
Jimena Arnaiz <jimeclaudeen@gmail.com>
2025-03-25 10:48:08 +0100

    Prueba con Sonarqube

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,11 +9,11 @@ plugins {
 
 sonarqube {
     properties {
-        property("sonar.projectKey", "com.example.recipeat")  // Ajusta esto a tu propio proyecto
+        property("sonar.projectKey", "com.example.recipeat")  // Mi proyecto
         property("sonar.projectName", "recipEat")
         property("sonar.projectVersion", "1.0")
         property("sonar.token", "squ_09a517edb1ff9b3da62d7947a66f5b94fe80b5db")
-        property("sonar.sources", listOf("src/main/java"))  // O la ruta correspondiente a tu código
+        property("sonar.sources", listOf("src/main/java"))  // O la ruta correspondiente a mi código
         property("sonar.tests", "src/androidTest/java")  // Rutas de tests
         property("sonar.java.binaries", "build/intermediates/classes/debug")  // Compilados del código
     }
