630de61a25fdbe809d55eb31ebe6145697f060d5
Jimena Arnaiz <jimeclaudeen@gmail.com>
2025-05-10 18:54:46 +0200

    para coverage

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,7 +90,7 @@ tasks.register<JacocoReport>("jacocoTestReport") {
 
     doNotTrackState("State tracking disabled for Jacoco report task")
 
-    val reportsDir = file("${layout.buildDirectory.get().asFile}/reports/jacoco/jacocoTestReport")
+    val reportsDir = file("app/build/reports/jacoco/jacocoTestReport")
 
     reports {
         xml.required.set(true)
